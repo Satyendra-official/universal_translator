00a5432c7c790f0e1bdacbe11e015c364598fbd8
Satyendra-official <satyendra.kr.official@gmail.com>
2025-05-03 17:24:14 +0530

    Bug fixed

--- app/output_handlers.py
+++ app/output_handlers.py
@@ -9,13 +9,3 @@ def text_to_speech(text, lang='es'):
     tts = gTTS(text=text, lang=lang)
     tts.save("static/translated_audio.mp3")
 
-    # Play audio based on operating system
-    system_platform = platform.system()
-    if system_platform == "Windows":
-        os.system("start translated_audio.mp3")
-    elif system_platform == "Darwin":  # macOS
-        os.system("afplay translated_audio.mp3")
-    elif system_platform == "Linux":
-        os.system("xdg-open translated_audio.mp3")
-    else:
-        print("Cannot auto-play audio on this OS.")
